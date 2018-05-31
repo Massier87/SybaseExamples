cca72c610a315bbf1b820493588970f4ae92521a
ariyapalak <messier0087@gmail.com>
2018-05-31 11:00:42 +0200

    two ways to create temporary tables

--- InterestingQuerries.sql
+++ InterestingQuerries.sql
@@ -18,14 +18,32 @@ and col2 IN ( select col2
 	group by col2 having count(col2) > 1
 )
 
---2)
+--2) Viewing table details
 Catalog stored procedure calls (CSPs)
 
 sp_tables:
 exec sp_tables '%', 'dbo', 'master', "'TABLE'" 
 exec sp_tables '%tablename%', 'dbo', 'master', "'TABLE | VIEW | SYSTEM TABLE | ALIAS'" 
 
-
 --References:
 --1) http://infocenter.sybase.com/help/index.jsp?topic=/com.sybase.infocenter.dc36525_1500/html/mfcig/CIHJEBIE.htm
 --2) http://razorsql.com/articles/sybase_admin_queries.html
+
+--3) Creating temporery tables
+CREATE TABLE tempdb..tmp_table_name (
+	col1 int,
+	col2 varchar(10)
+)
+
+INSERT INTO tempdb..tmp_table_name
+SELECT *
+FROM tableA
+
+-- or
+
+SELECT *
+INTO #tmp_table_name
+FROM tableA
+
+-- First option the table will remain even after the currnet connection is closed.
+-- Whereas in the second method the temporery table will exist only during the connection.
